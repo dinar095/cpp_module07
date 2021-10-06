8c204917d7937c98e7e442b5f65648270c1d6dbb
Dinar Teregulov <dinar095@hotmail.com>
2021-10-06 08:41:06 +0300

    ex01 done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,11 +1,18 @@
 #ifndef CPP_MODULE07_ITER_HPP
 #define CPP_MODULE07_ITER_HPP
+#include <iostream>
 
 template <typename T>
 void iter(T* arr, int len, void(*f)(const T& arg))
 {
 	for (int i = 0; i < len; i++)
 		f(arr[i]);
+}
+
+template <typename T>
+void print(T& arg)
+{
+	std::cout << arg << std::endl;
 }
 
 #endif //CPP_MODULE07_ITER_HPP
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,5 +2,6 @@
 
 int main()
 {
-
+	int arr[] = {1, 65, 85, 75, 68, 78};
+	::iter<int>(arr, 6, print);
 }
